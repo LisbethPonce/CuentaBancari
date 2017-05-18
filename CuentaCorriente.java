d78c9f600b7fac9ac2450b9f2401a415592d3a09
LisbethPonce <Lisbethponce97@outlook.es>
2017-05-18 17:46:25 -0500

    Add files via upload

---
+++ CuentaCorriente.java
@@ -0,0 +1,21 @@
+
+package cuentabancaria;
+
+
+public class CuentaCorriente extends CuentaBancaria {
+
+    @Override
+    double getinteresDeposito() {
+        return 0.02;
+    }
+    
+    /*@Override
+    public void deposito(double deposito){
+        double interes=0;
+        if(deposito<1000)
+            interes=deposito*0.01;
+        
+        this.Saldo=this.Saldo+deposito+interes;    
+    }*/
+    
+}
